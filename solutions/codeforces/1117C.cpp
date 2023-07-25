fd08850fa4cc38ce37536376ef39a8859a78627b
Colin D <colinding10@gmail.com>
2023-07-25 16:29:18 -0400

    alksjflakjf

--- solutions/codeforces/1117C.cpp
+++ solutions/codeforces/1117C.cpp
@@ -1,21 +1,4 @@
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
