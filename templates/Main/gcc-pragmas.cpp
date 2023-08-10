7b413e91a7d599520e25d35c35f46dd19fd89ea4
Colin D <colinding10@gmail.com>
2023-08-10 11:26:25 -0400

    update pragmas

--- templates/Main/gcc-pragmas.cpp
+++ templates/Main/gcc-pragmas.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
 
 #pragma comment(linker, "/stack:200000000")
-#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC optimize("Ofast", "unroll-loops")
 #pragma GCC target("avx2", "popcnt")
