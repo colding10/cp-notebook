63c5098506806f0aa70fabf1ce2c0400fde3b320
Colin D <colinding10@gmail.com>
2023-08-10 11:26:11 -0400

    more sols

---
+++ solutions/codeforces/1857E.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using str = string;
+using db = long double;
+
+template <class T> using V = vector<T>;
+template <class T, class U> using P = pair<T, U>;
+template <class T, size_t SZ> using AR = array<T, SZ>;
+
+using pi = P<int, int>;
+using pll = P<ll, ll>;
+
+using vi = V<int>;
+using vll = V<ll>;
+using vb = V<bool>;
+using vpi = V<pi>;
+using vpll = V<pll>;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define endl "\n"
+
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+
+#define rep(i, begin, end)                                                     \
+    for (__typeof(end) i = (begin) - ((begin) > (end));                        \
+         i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+
+inline namespace FileIO {
+void setIn(const str &s) { freopen(s.c_str(), "r", stdin); }
+void setOut(const str &s) { freopen(s.c_str(), "w", stdout); }
+void setIO(const str &s = "") {
+    cin.tie(0)->sync_with_stdio(0);
+    if (sz(s)) setIn(s + ".in"), setOut(s + ".out");
+}
+} // namespace FileIO
+
+inline namespace Debug {
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+    cerr << '{';
+    __print(x.first);
+    cerr << ", ";
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T> void __print(const T &x) {
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+    __print(t);
+    if (sizeof...(v)) cerr << ", ";
+    _print(v...);
+}
+
+#ifndef ONLINE_JUDGE
+#define dbg(x...)                                                              \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = [";  \
+    _print(x);                                                                 \
+    cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx2", "popcnt")
+#endif
+} // namespace Debug
+#define int ll
+void solve() {
+    int n;
+    cin >> n;
+    vpi a(n);
+    rep(i, 0, n) {
+        cin >> a[i].fi;
+        a[i].se = i;
+    }
+
+    sort(all(a));
+
+    vi intervals(n - 1);
+    rep(i, 0, n - 1) {
+        intervals[i] = a[i + 1].fi - a[i].fi;
+    }
+
+    vpi out(n);
+
+    rep(i, 0, n) {
+        int sum = n;
+        int mult = 1;
+        rep(j, 0, i) {
+            sum += mult * intervals[j];
+            mult++;
+        }
+        mult = 1;
+        for (int j = n - 1; j > i; j--) {
+            sum += mult * intervals[j - 1];
+            mult++;
+        }
+        out[i] = mp(a[i].se, sum);
+    }
+    sort(all(out));
+    rep(i, 0, n) {
+        cout << out[i].se << " ";
+    }
+    cout << endl;
+}
+
+int32_t main() {
+    setIO();
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    // you should actually read the stuff at the bottom
+}
+
+// keep calm
+// stay organized
+// do something instead of wasting time
+// the solution is probably simpler than you think
