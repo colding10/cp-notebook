c725108c1956f4219d65aa6b0e624c7e58c0e005
Colin Ding <colinding10@gmail.com>
2026-01-25 15:58:27 -0500

    update

--- solutions/usaco2025mar/orzbenq.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cout << "benq these usaco contests are so hard man" << endl;
-    return 1; // 1! because there is a error, in my brain
-}
--- solutions/usaco2025mar/troll.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cout << "3\n2 1 \n33 5\n10 5" << endl;
-    return 1;
-}
