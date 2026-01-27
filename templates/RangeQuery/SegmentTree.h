1acb8f804f55f82b4bb4da364e8d120d72081b8d
Colin Ding <colinding10@gmail.com>
2026-01-27 15:09:01 -0500

    update

--- templates/RangeQuery/SegmentTree.h
+++ templates/RangeQuery/SegmentTree.h
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
 template <typename T> struct SegTree {
     static constexpr T unit = INT_MIN;
-    T f(T a, T b) { return std::max(a, b); } // (any associative fn)
-    std::vector<T> s;
-    int n;
+    T f(T a, T b) { return max(a, b); } // (any associative fn)
+    vector<T> s;
+    ll n;
 
-    SegTree(int n = 0, T def = unit) : s(2 * n, def), n(n) {}
-    void update(int pos, T val) {
+    SegTree(ll n = 0, T def = unit) : s(2 * n, def), n(n) {}
+    void update(ll pos, T val) {
         for (s[pos += n] = val; pos /= 2;)
             s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
     }
-    T query(int b, int e) { // query [b, e)
+    T query(ll b, ll e) { // query [b, e)
         T ra = unit, rb = unit;
         for (b += n, e += n; b < e; b /= 2, e /= 2) {
             if (b % 2) ra = f(ra, s[b++]);
